d0b4dbdba99aad6ca96cd824263c9eb2553da1b3
Samuel Pitoňák <samuel.pitonak@student.tuke.sk>
2018-10-21 21:07:10 +0200

    StopwatchActivity added

---
+++ app/src/main/java/sk/tuke/fei/kpi/smart/makac/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package sk.tuke.fei.kpi.smart.makac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
