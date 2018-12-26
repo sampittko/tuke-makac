b1034f0a8ac822f7ad9ecfe84a1def6aafb6a4e8
Samuel Pitoňák <samuel.pitonak@student.tuke.sk>
2018-12-26 16:00:57 +0100

    save changes button default value: GONe

--- app/src/main/java/sk/tuke/smart/makac/LoginActivity.java
+++ app/src/main/java/sk/tuke/smart/makac/LoginActivity.java
@@ -56,7 +56,6 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         configureOnClickListeners();
         configureSignOutButton();
-        saveChangesButton.setVisibility(View.VISIBLE);
     }
 
     private void configureOnClickListeners() {
